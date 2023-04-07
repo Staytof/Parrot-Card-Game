af2a8f45005af48494816fbb98845030e5fe1a10
Gabriel Santos Dias <Gabrielspec99@gmail.com>
2023-04-07 19:09:21 -0300

    att projeto

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
+//Prompt para numero de cartas
+
 let numCards = parseInt(prompt('Com quantas cartas você quer jogar? (Digite um número par entre 4 e 14)', ''));
 
 while (isNaN(numCards) || numCards % 2 !== 0 || numCards < 4 || numCards > 14) {
     numCards = parseInt(prompt('Número inválido. Com quantas cartas você quer jogar? (Digite um número par entre 4 e 14)', ''));
 }
+
+//Array com todas as cartas
 
 const grid = document.querySelector('.grid');
 const parrots = [
@@ -14,6 +18,8 @@ const parrots = [
     'tripletsparrot',
     'unicornparrot'
 ];
+
+//Função para criar as cartas no HTML
 
 const createCard = (parrot) => {
     const card = document.createElement('div');
@@ -34,21 +40,52 @@ const createCard = (parrot) => {
     card.appendChild(front);
     card.appendChild(back);
 
+    card.addEventListener('click', revealCard);
+
     return card;
 };
+
+//Função para virar as cartas
+
+const revealCard = (event) => {
+    const card = event.target;
+    const frontFace = card.querySelector('.front-face');
+    const backFace = card.querySelector('.back-face');
+    const frontImg = frontFace.querySelector('img');
+    const backImg = backFace.querySelector('img');
+
+    frontFace.style.pointerEvents = 'auto';
+    backFace.style.pointerEvents = 'auto';
+
+    frontImg.src = backImg.src;
+    backImg.src = frontImg.src;
+};
+
+
+//Função para embaralhar as cartas
 
 const comparador = () => {
     return Math.random() - 0.5;
 };
 
-const loadGame = (numCards) => {
+const flipCard = (event) => {
+    const card = event.currentTarget;
+    card.classList.add('flipped');
+};
 
+const loadGame = (numCards) => {
     grid.innerHTML = '';
 
     const selectedParrots = parrots.sort(comparador).slice(0, numCards / 2);
     const parrotsDuplicates = [...selectedParrots, ...selectedParrots];
     parrotsDuplicates.sort(comparador).forEach((parrot) => {
         const card = createCard(parrot);
+        card.addEventListener('click', flipCard);
+        card.addEventListener('click', (event) => {
+            setTimeout(() => {
+                flipCard(event);
+            }, 1000);
+        });
         grid.appendChild(card);
     });
 };
